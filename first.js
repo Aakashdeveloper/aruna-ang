2f23574633296ac24c912639cba56318d804233e
AAKASH HANDA <AAKASH HANDA>
2017-08-14 00:12:58 +0530

    A

---
+++ first.js
@@ -0,0 +1,92 @@
+var a = "string"
+var b = 516551
+var c = true;
+
+var a = "i am doing javascript"
+undefined
+var b ="with nareshIt"
+undefined
+a+b
+"i am doing javascriptwith nareshIt"
+a.concat(b)
+"i am doing javascriptwith nareshIt"
+
+
+10+10
+20
+10+10+"10"
+"2010"
+"10"+10+10
+"101010"
+10+"10"+10
+"101010"
+
+10
+10=="10"
+true
+10==="10"
+false
+10===10
+true
+
+
+var a ="aruna"
+undefined
+a.length
+5
+a.toUpperCase()
+"ARUNA"
+a.toLowerCase()
+"aruna"
+
+var a = "aruna is doing java"
+undefined
+a.replace("java","javascript")
+"aruna is doing javascript"
+a.length
+19
+a.replace(" ","-")
+"aruna-is doing java"
+a.replace(/ /g,"-")
+"aruna-is-doing-java"
+
+var a = "  aakash  is teaching "
+var a = "  aakash  is teaching "
+undefined
+a.trim().replace(/ /g,"-")
+"aakash--is-teaching"
+
+
+var a = "  aakash  is teaching "
+undefined
+a.trim().replace(/ /g,"-")
+"aakash--is-teaching"
+a.trim().replace(/ /g,"-").toUpperCase()
+"AAKASH--IS-TEACHING"
+
+
+document.URL
+"https://wf30.myhcl.com/gis/Forms/Employee/final-submission.aspx"
+var a = document.URL
+undefined
+a.split("/")
+
+
+var a = "aruna is doing javascript"
+undefined
+a.slice(9,5)
+""
+a
+"aruna is doing javascript"
+a.slice(9,12)
+"doi"
+a.slice(9,15)
+"doing "
+
+
+var a = "aruna is doing javascript"'
+VM264:1 Uncaught SyntaxError: Invalid or unexpected token
+var a = "aruna is doing javascript"
+undefined
+a.indexOf("doing")
+9ss
---
+++ ifelse.js
@@ -0,0 +1,50 @@
+var a = 31631654;
+
+if(a%2 == 0){
+    console.log("num is even")
+}else if(a%3 ==0){
+    console.log("num is divisible by 3")
+}
+else if(a%5 ==0){
+    console.log("num is divisible by 3")
+}
+else{
+    console.log("num is odd")
+}
+
+
+if(a%2==0 || a%3==0){
+    console.log("num is dividble")
+}else{
+    console.log("num is not divisble")
+}
+
+if(a%2==0 && a%3==0){
+    console.log("num is dividble")
+}else{
+    console.log("num is not divisble")
+}
+
+var name = 
+
+if(name=="aruna"){
+    console.log("allow access")
+}else if(name == "aakash"){
+    console.log("donot allow access")
+}else{
+    console.log("i dont know")
+}
+
+var name = 
+if(name.length < 10){
+    console.log("allow access")
+}else{
+     console.log("donot allow access
+}
+
+var phone= "9643018019"
+if(isNaN(phone)==false && phone.length == 10){
+    console.log("number is ok")
+}else{
+    console.log("number is invalid")
+}

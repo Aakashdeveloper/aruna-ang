085e18e3959fabaa8c759de6af598610d84f43d3
AAKASH HANDA <AAKASH HANDA>
2017-08-15 23:39:02 +0530

    A

---
+++ function.js
@@ -0,0 +1,44 @@
+function add(a,b){
+    return a+b
+}
+
+add(465,656)
+
+function aruna(name,classA,roll){
+    var result = "my name is "+name+"class is "+classA+"roll is"+roll;
+    return result
+}
+
+function isEven(a){
+    if(a%2==0){
+        console.log("number is even")
+    }else{
+        console.log("number is odd")
+    }
+}
+
+function valid(r){
+        for(i=0;i<=r.length;i++){
+            if(r[i]>6){
+                console.log(r[i])
+            }
+        }
+}
+
+var valid = function(r)
+{
+    for(i=0;i<=r.length;i++){
+            if(r[i]>6){
+                console.log(r[i])
+            
+}
+
+Es6
+var valid = (r) =>{
+    for(i=0;i<=r.length;i++){
+            if(r[i]>6){
+                console.log(r[i])
+            
+}
+
+var a = [["aakash","male",10],["anchal","female",10],["jassi","male",12]]
---
+++ test.js
@@ -0,0 +1,38 @@
+string
+number 
+boolean
+null
+undefined
+
+str a ="fe"
+int a =10
+bool a = true
+
+Es5
+var a = "javascript"
+var a =10.878
+var a =true
+
+Es6
+let d = 10
+undefined
+d
+10
+let d = 10
+const a =10
+
+var a = "manoranjan"
+a.length
+a
+"manoranjan"
+a.length
+10
+a.toUpperCase()
+"MANORANJAN"
+a.toLowerCase()
+"manoranjan"
+
+10+10+"10"
+"2010"
+"10"+10+10
+"101010"

9791124872c74efa7c9ac0332d36ffca55556f45
AAKASH HANDA <AAKASH HANDA>
2017-08-17 23:48:45 +0530

    A

---
+++ object.js
@@ -0,0 +1,38 @@
+var animal = {
+    name:"dog",
+    age:20,
+    setage = function(a){
+      age =a    }
+}
+
+undefined
+animal
+{name: "dog", age: 20}
+animal.name
+"dog"
+animal.age
+20
+animal.color="red"
+"red"
+animl
+animal
+{name: "dog", age: 20, color: "red"}
+delete animal.age
+true
+animal
+{name: "dog", color: "red"}
+
+
+
+var person = new Object()
+undefined
+person
+{}
+person.name="aakash"
+"aakash"
+persone.age = 10
+person.age = 10
+10
+persone
+person
+{name: "aakash", age: 10}
